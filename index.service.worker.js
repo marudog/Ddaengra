0a0998ffed2c3dc8941b3c4719a8dde6cd30f878
marudog <59045847+marudog@users.noreply.github.com>
2025-07-24 17:36:18 +0900

    canvas

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753345957|10342110909';
+const CACHE_VERSION = '1753346170|10555066360';
 /** @type {string} */
 const CACHE_PREFIX = 'Flappy Bird Clon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
